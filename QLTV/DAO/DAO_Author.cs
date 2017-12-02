06b65a899ee4d0bb8b4da17095f96059bbb69436
Khánh Quốc <kpmquockhanh@gmail.com>
2017-12-02 10:40:59 +0700

    ok

--- QLTV/DAO/DAO_Author.cs
+++ QLTV/DAO/DAO_Author.cs
@@ -3,11 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using DTO;
+using System.Data;
 namespace DAO
 {
-    public class DAO_Author
+    public class DAO_Author:Connection
     {
-        
+        public int Insert(DTO_Book book)
+        {
+            return 0;
+        }
+
+        public int Update(DTO_Book book)
+        {
+            return 0;
+        }
+
+        public int Delete(string b_id)
+        {
+            return 0;
+        }
+
+        public DataTable GetAllDataTable()
+        {
+            return this.getTable("Select * from Author");
+        }
+
+        public DataTable GetDataTableBy(String condition, String value)
+        {
+            return this.getTable("select * from AuThor where " + condition + " = '" + value + "'");
+        }
     }
 }
