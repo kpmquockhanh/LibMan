29a6119971fae303228369773ea2386fc71dc8ba
thantam03011997@gmail.com <thantam03011997@gmail.com>
2017-12-04 20:29:54 +0700

    ahuhu

--- QLTV/GUI/Program.cs
+++ QLTV/GUI/Program.cs
@@ -16,7 +16,7 @@ namespace GUI
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmLogin());// đưng
+            Application.Run(new frmLogin());// đưng ko có cách lh
         }
     }
 }
